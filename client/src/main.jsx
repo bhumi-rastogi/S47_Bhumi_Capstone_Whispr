7232efa93021da1040e98cb765af7b0cc637c340
bhumi <bhumi.rastogi05@gmail.com>
2024-03-14 11:25:00 +0530

    importingCssBaseline

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {CSSBaseline} from '@mui/icon/material'; 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CSSBaseline/>
     <App />
   </React.StrictMode>,
 )
