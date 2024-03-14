4f0c1a5835e2c4f986192a8d386b0cf1aa3a5f8e
bhumi <bhumi.rastogi05@gmail.com>
2024-03-14 11:34:26 +0530

    creating routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<p>HomePage</p>}/>
+      <Route path='about' element={<p>About</p>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
