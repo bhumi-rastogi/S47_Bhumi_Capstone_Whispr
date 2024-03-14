bc4d720ccaed8bc91d0c27a099a73581cee5db89
bhumi <bhumi.rastogi05@gmail.com>
2024-03-14 15:13:34 +0530

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{lazy} from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+const Home= lazy(() => import("./pages/Home"));
+const Login= lazy(() => import("./pages/Login"));
+const Chat = lazy(()=>import("./pages/Chat"))
+const Groups = lazy(()=>import("./pages/Groups"))
+// dynamic import jabh jarurat hogi tabhi import hoga
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<p>HomePage</p>}/>
-      <Route path='about' element={<p>About</p>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/chat/:chatId'element={< Chat/>}/>
+      <Route path='/login'element={<Login/>}/>
+      <Route path='/groups'element={<Groups/>}/>
+      
     </Routes>
     </BrowserRouter>
   )
